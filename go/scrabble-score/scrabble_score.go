adff54ffa2a76a04f7a91e9b7b57c73cee2d077a
jwosborn <osbornjeremyw@yahoo.com>
2020-04-21 11:30:10 -0400

    scrabble improvements

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,42 +1,29 @@
 package scrabble
 
-import "strings"
-
-var m = map[string]int{
-	"A": 1,
-	"B": 3,
-	"C": 3,
-	"D": 2,
-	"E": 1,
-	"F": 4,
-	"I": 1,
-	"O": 1,
-	"U": 1,
-	"L": 1,
-	"N": 1,
-	"R": 1,
-	"S": 1,
-	"T": 1,
-	"G": 2,
-	"M": 3,
-	"P": 3,
-	"V": 4,
-	"W": 4,
-	"Y": 4,
-	"H": 4,
-	"K": 5,
-	"J": 8,
-	"X": 8,
-	"Q": 10,
-	"Z": 10,
-}
+import (
+	"unicode"
+)
 
 // Score iterates over a string and assigns a score to the string based on scrabble scoring
 func Score(a string) int {
 	score := 0
 	for _, r := range a {
-		letter := string(r)
-		score += m[strings.ToUpper(letter)]
+		switch unicode.ToUpper(r) {
+		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
+			score++
+		case 'D', 'G':
+			score += 2
+		case 'B', 'C', 'M', 'P':
+			score += 3
+		case 'F', 'H', 'V', 'W', 'Y':
+			score += 4
+		case 'K':
+			score += 5
+		case 'J', 'X':
+			score += 8
+		case 'Q', 'Z':
+			score += 10
+		}
 	}
 	return score
 }
