c8f0e69796ec1d2ddc73bc029a97dee899f88417
jwosborn <osbornjeremyw@yahoo.com>
2020-10-23 16:28:37 -0400

    word-count

--- elixir/word-count/lib/word_count.ex
+++ elixir/word-count/lib/word_count.ex
@@ -6,5 +6,27 @@ defmodule WordCount do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
+    sentence
+    |> to_string
+    |> String.downcase()
+    |> String.replace(~r/[^\w\d\s_-]/u, "")
+    |> String.split(
+      ~r/_|\s/,
+    trim: true
+    )
+    |> Enum.reduce(%{}, &counter/2)
+  end
+
+  def counter(str, acc) do
+    Map.get(acc, str)
+    |> has(str, acc)
+  end
+
+  def has(nil, key, acc) do
+    Map.put(acc, key, 1)
+  end
+
+  def has(value, key, acc) do
+    Map.put(acc, key, value + 1)
   end
 end
